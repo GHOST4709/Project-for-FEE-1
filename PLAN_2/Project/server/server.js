085ec4c4a36d74bf79214ebdd1c2581566061fd3
Abhishek Sharma <abhishek.sharma.988859@gmail.com>
2026-04-23 15:08:06 +0530

    Server Hand fixed and now waiting for the css and js fix

--- PLAN_2/Project/server/server.js
+++ PLAN_2/Project/server/server.js
@@ -1,44 +1,82 @@
-import { error } from 'node:console';
-import http, { get } from 'node:http' 
-
+import http from 'node:http';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 const PORT = 8000;
 
+// const server = http.createServer(async (req,res)=>{
+    
+//     const urlObj = new URL(req.url, `http://${req.headers.host}`)
+    
+//     if(urlObj.pathname.startsWith('/api') && req.method === "GET" ){
+//         res.setHeader("Content-Type", "application/json");
+//         res.setHeader('Access-Control-Allow-Origin', '*')
+//         res.setHeader('Access-Control-Allow-Methods', 'GET')
+//         const filePath = path.join(__dirname,'../Login/login.html');
+//             fs.readFile(filePath, (err, content) => {
+//                 if (err) {
+//                     res.writeHead(500, { "Content-Type": "text/plain" });
+//                     res.end("Server Error: Could not read the HTML file.");
+//                 } else {
+//                     res.writeHead(200, { "Content-Type": "text/html" });
+//                     res.end(content);
+//                 }
+//             });
+//         res.statusCode = 200;
+//         res.end();
+//     }
+//     else{
+//         res.setHeader("Content-Type", "application/json");
+//         res.statusCode = 404
+//         const message = {
+//             error: "Not Found",
+//             message: "This request route does not exist."
+//         }
+//         res.end(JSON.stringify(message))
+//     }
+// });
 
-const server = http.createServer(async (req,res)=>{
+
+// server.listen(PORT,()=>{
+//     console.log(`The Server is Running on PORT ${PORT}`);
+//     console.log("http://localhost:8000");
+// })
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const server = http.createServer((req, res) => {
+    const urlObj = new URL(req.url, `http://${req.headers.host}`);
     
-    const urlObj = new URL(req.url, `http://${req.headers.host}`)
-    
-    if(urlObj.pathname.startsWith('/api') && req.method === "GET" ){
-        res.setHeader("Content-Type", "application/json");
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        res.setHeader('Access-Control-Allow-Methods', 'GET')
-        const filePath = path.join(__dirname,'../Login/login.html');
-            fs.readFile(filePath, (err, content) => {
-                if (err) {
-                    res.writeHead(500, { "Content-Type": "text/plain" });
-                    res.end("Server Error: Could not read the HTML file.");
-                } else {
-                    res.writeHead(200, { "Content-Type": "text/html" });
-                    res.end(content);
-                }
-            });
-        res.statusCode = 200;
+    if ((urlObj.pathname === '/' || urlObj.pathname === '/login') && req.method === "GET") {
+        const filePath = path.join(__dirname, '../Login/login.html');
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end("Server Error: Could not read the HTML file.");
+            } else {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(content);
+            }
+        });
+    } 
+    else if (urlObj.pathname.startsWith('/api') && req.method === "GET") {
+        res.writeHead(200, {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET"
+        });
         res.end();
-    }
-    else{
-        res.setHeader("Content-Type", "application/json");
-        res.statusCode = 404
-        const message = {
+    } 
+    else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({
             error: "Not Found",
             message: "This request route does not exist."
-        }
-        res.end(JSON.stringify(message))
+        }));
     }
 });
 
-
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`The Server is Running on PORT ${PORT}`);
     console.log("http://localhost:8000");
-})
-
+});
