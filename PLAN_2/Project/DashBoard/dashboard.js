2c454be096b874a4feeab4192daeb5a4eb0e386f
Abhishek Sharma <abhishek.sharma.988859@gmail.com>
2026-04-26 20:47:55 +0530

    Good to go with the Chess Api wtih working and now waiting for final optimisations

--- PLAN_2/Project/DashBoard/dashboard.js
+++ PLAN_2/Project/DashBoard/dashboard.js
@@ -17,13 +17,15 @@ function logout() {
 
 // 3.Memes & Quotes
 const subtleMemes = [
-    "It’s not a bug, it’s an undocumented feature. 🐛",
+    "It’s not a bug, it’s just a soon to be Feature.",
     "Lag is my only real enemy. 📶",
     "I don't need to save, I like to live dangerously. (Saves 5 times anyway)",
-    "My code worked yesterday. I haven't changed anything. Today it's broken. 🤷‍♂️",
+    "My code worked yesterday. I haven't changed anything. Today it's broken.",
     "There are 10 types of people: those who understand binary, and those who don't.",
     "First rule of programming: If it works, don't touch it. ⚠️",
-    "Me: *places a single block in Minecraft*. Teacher: Architecture! 🏛️"
+    "Me: *places a single block in Minecraft*. Teacher: Architecture!",
+    "A Wise Man Once said: What?",
+    "A Junior Intern Once said: Why not Use a forLoop to Search a DB with millons of users?"
 ];
 
 function loadDailyDrop() {
