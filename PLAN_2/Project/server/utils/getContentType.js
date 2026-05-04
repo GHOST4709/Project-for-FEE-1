8a5c7bf16aba24b6ecbbc3fb5e0f997cf3ba2357
Abhishek Sharma <abhishek.sharma.988859@gmail.com>
2026-05-04 09:41:31 +0530

    .

--- PLAN_2/Project/server/utils/getContentType.js
+++ PLAN_2/Project/server/utils/getContentType.js
@@ -1,19 +1,21 @@
-// import path from "node:path";
+import path from "node:path";
 
-export default function getContentType(ext) {
+export default function getContentType(filePath) {
+    // Extract just the extension from the full file path
+    const ext = String(path.extname(filePath)).toLowerCase();
 
     const types = {
-    ".js": "text/javascript",
-    ".css": "text/css",
-    ".json": "application/json",
-    ".png": "image/png",
-    ".jpg": "image/jpeg",
-    ".jpeg": "image/jpeg",
-    ".gif": "image/gif",
-    ".svg": "image/svg+xml"
-    }
+        ".html": "text/html",  // <-- I forgot this line last time!
+        ".js": "text/javascript",
+        ".css": "text/css",
+        ".json": "application/json",
+        ".png": "image/png",
+        ".jpg": "image/jpeg",
+        ".jpeg": "image/jpeg",
+        ".gif": "image/gif",
+        ".svg": "image/svg+xml"
+    };
     
-    return types[ext.toLowerCase()] || "text/html"
+    // Return the correct type, or default to a generic download
+    return types[ext] || "application/octet-stream";
 }
-
-
